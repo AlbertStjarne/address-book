191020af284d74eb81d514a28f62618a14aefdd4
Shirmen <shirmenchen@gmail.com>
2018-09-04 14:55:35 +0200

    add features

--- features/step_defintions/basic-steps.js
+++ features/step_defintions/basic-steps.js
@@ -1,9 +1,21 @@
-const { Given, Then } = require("cucumber");
+const { Given, When, Then } = require("cucumber");
 
 Given("I visit the site", function() {
   return "pending";
 });
-
 Then("I should see {string}", function(string) {
   return "pending";
+});
+
+When("I click {string}", async function(string){
+  return "pending"
+});
+Then("I fill in {string} with {string}", async function(string1, string2){
+  return "pending"
+});
+Then("I should have {int} contact in my address book", async function(int){
+  return "pending"
+});
+Then("I should not see {string}", async function(string){
+  return "pending"
 });
